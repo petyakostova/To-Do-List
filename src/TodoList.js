3df4248e4f386c737a452a64e8397a2125ac1f0c
petyakostova <petyakostova555@gmail.com>
2019-06-20 18:37:27 +0300

    testing => adding items done

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,46 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+        }
+
+        this._inputElement.value="";
+
+        console.log(this.state.items);
+
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div className="todoListMain">
                 <div className="header">
-                    <form>
-                        <input placeholder="enter task"></input>
-                        <button type="submit">add</button>
+                    {/* onSubmit event => when the form is submited, the addItem method will be called */}
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a}
+                            placeholder="Enter task"></input>
+                        <button type="submit">Add</button>
                     </form>
                 </div>
             </div>
